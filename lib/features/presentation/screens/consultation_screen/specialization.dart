ef4d7ecfa3a2a2921239aa16fe28e06bade84656
Rizqi <rizqiaditya222@gmail.com>
2025-03-12 02:08:53 +0700

    slicing specialization done

--- lib/features/presentation/screens/consultation_screen/specialization.dart
+++ lib/features/presentation/screens/consultation_screen/specialization.dart
@@ -66,85 +66,128 @@ class _SpecializationState extends State<Specialization> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Align(
-          alignment: Alignment.center, 
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("Pilih Spesialisasi", style: Theme.of(context).textTheme.headlineLarge, textAlign: TextAlign.center,),
-              Text("Pilih Spesialisasi Psikolog Mau konsultasi dengan psikolog yang sesuai? Pilih spesialisasi yang paling cocok untukmu!", style: TextStyle(color: Color(0xFF3D3B3B), fontSize: 20), textAlign: TextAlign.center,),
-              SizedBox(
-                width: 350, 
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 2.8,
-                    crossAxisSpacing: 16,
-                    mainAxisSpacing: 16,
+              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+
+              Text(
+                "Pilih Spesialisasi",
+                style: Theme.of(context).textTheme.headlineLarge,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 48),
+              Text(
+                "Pilih Spesialisasi Psikolog Mau konsultasi dengan psikolog yang sesuai? Pilih spesialisasi yang paling cocok untukmu!",
+                style: TextStyle(color: Color(0xFF3D3B3B), fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              Center(
+                child: SizedBox(
+                  width: 350,
+                  child: GridView.builder(
+                    shrinkWrap: true, // Pastikan GridView menyesuaikan dengan kontennya
+                    physics: NeverScrollableScrollPhysics(), // Hindari konflik dengan SingleChildScrollView
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 2.8,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                    ),
+                    itemCount: options.length,
+                    itemBuilder: (context, index) {
+                      bool isSelected = _selectedIndex == index;
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selectedIndex = index;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: isSelected ? Color(0xFFF3BB06) : Colors.white,
+                            borderRadius: BorderRadius.circular(50),
+                            boxShadow: [
+                              BoxShadow(
+                                color: isSelected
+                                    ? Color(0x40E3C896)
+                                    : const Color.fromARGB(255, 241, 241, 241),
+                                spreadRadius: 5,
+                                blurRadius: isSelected ? 0 : 10,
+                                offset: isSelected ? Offset(0, 0) : Offset(0, 5),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 48,
+                                height: 48,
+                                decoration: BoxDecoration(
+                                  color: isSelected ? Color(0xFFF5D161) : primary[90],
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Image(
+                                  image: isSelected
+                                      ? options[index]['activeIcon']
+                                      : options[index]['icon'],
+                                  width: 40,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(width: 12),
+                              Text(
+                                options[index]['text'],
+                                style: TextStyle(
+                                  color: isSelected ? Colors.white : Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                  itemCount: options.length,
-                  itemBuilder: (context, index) {
-                    bool isSelected = _selectedIndex == index;
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _selectedIndex = index;
-                        });
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: isSelected ? Color(0xFFF3BB06) : Colors.white,
-                          borderRadius: BorderRadius.circular(50),
-                          boxShadow: [
-                            BoxShadow(
-                              color: isSelected
-                                  ? Color(0x40E3C896)
-                                  : const Color.fromARGB(255, 241, 241, 241),
-                              spreadRadius: 5,
-                              blurRadius: isSelected ? 0 : 10,
-                              offset: isSelected ? Offset(0, 0) : Offset(0, 5),
-                            ),
-                          ],
-                        ),
-                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 48,
-                              height: 48,
-                              decoration: BoxDecoration(
-                                color: isSelected ? Color(0xFFF5D161) : primary[90],
-                                shape: BoxShape.circle,
-                              ),
-                              child: Image(
-                                image: isSelected
-                                    ? options[index]['activeIcon']
-                                    : options[index]['icon'],
-                                width: 40,
-                                color: Colors
-                                    .white, // Jika ingin mewarnai gambar, gunakan ColorFilter
-                              ),
-                            ),
-                            SizedBox(width: 12),
-                            Text(
-                              options[index]['text'],
-                              style: TextStyle(
-                                color: isSelected ? Colors.white : Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
                 ),
               ),
+
+              SizedBox(height: 48), // Jarak antara GridView dan tombol
+
+              // Tombol "Cari Ahli"
+              SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    backgroundColor: primary[90],
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Cari Ahli",
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(width: 16),
+                      Icon(Icons.search, color: Colors.white),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
             ],
           ),
         ),
